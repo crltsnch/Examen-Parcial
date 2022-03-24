ab8e5549be1b71bf64d73f90553ca722141d3be1
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-03-24 09:24:52 +0100

    Ejercicio 4, clase con sus atributos

---
+++ Ejercicio 4.py
@@ -0,0 +1,8 @@
+class CuentaBancaria():
+    def __init__ (self, ID, titular, fecha, ndecuenta, saldo):
+        self.ID = ID
+        self.titular = titular
+        self.fecha = fecha
+        self.ndecuenta = ndecuenta
+        self.saldo = saldo
+        
