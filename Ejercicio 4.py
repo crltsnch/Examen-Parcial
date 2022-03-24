7073fde76622dcdd69b8314b012c8e69e0969e2c
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-03-24 09:53:51 +0100

    Ejercicio 4, funcion transferir

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -6,16 +6,25 @@ class CuentaBancaria():
         self.ndecuenta = ndecuenta
         self.saldo = saldo
 
-#ingresar, retirar, transferir
-def RetirarDinero(self, reintegro):
-    if self.saldo > reintegro:
-        self.saldo = self.saldo - reintegro
+    #ingresar, retirar, transferir
+    def RetirarDinero(self, reintegro):
+        reintegro = int(input("¿Qué cantidad de dinero desea retirar? "))
+        if self.saldo > reintegro:
+            self.saldo = self.saldo - reintegro
+            print("Su nuevo saldo es " + self.saldo)
+        else:
+            print("No hay saldo suficiente")
+
+    def IngresarDinero(self, ingreso):
+        ingreso = int(input("¿Qué cantidad de dinero desea ingresar? "))
+        self.saldo = self.saldo + ingreso
         print("Su nuevo saldo es " + self.saldo)
 
-    else:
-        print("No hay saldo suficiente")
-
-def IngresarDinero(self, ingreso):
-    self.saldo = self.saldo + ingreso
-    print("Su nuevo saldo es " + self.saldo)
+    def TransferirDinero(self, transferido):
+        transferido = int(input("¿Qué cantidad de dinero desea transferir?"))
+        if self.saldo > transferido:
+            self.saldo = self.saldo - transferido
+            print("Su nuevo saldo es " + self.saldo)
+        else:
+            print("No hay saldo suficiente")
     
